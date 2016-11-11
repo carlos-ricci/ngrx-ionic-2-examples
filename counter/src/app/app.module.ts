487d1f8356df5098c72b0f5e5472981f8cc78a5d
Rafael Sanchez <rafasanmartinez@gmail.com>
2016-11-11 16:49:40 +0100

    Modify counter.ts so it works with ngc

--- counter/src/app/app.module.ts
+++ counter/src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
 import { StoreModule } from '@ngrx/store';
 import { counterReducer } from '../reducers/counter';
 
--- counter/src/reducers/counter.ts
+++ counter/src/reducers/counter.ts
@@ -1,9 +1,10 @@
-import {ActionReducer, Action} from "@ngrx/store";
-
 /*
     Default parameter will be used for initial state unless initial
     state is provided for this reducer in 'provideStore' method.
- */
+*/
+
+/*
+import {ActionReducer, Action} from "@ngrx/store";
 export const counterReducer: ActionReducer<number> = (state: number = 0, action: Action) => {
     switch(action.type){
         case 'INCREMENT':
@@ -14,3 +15,25 @@ export const counterReducer: ActionReducer<number> = (state: number = 0, action:
             return state;
     }
 };
+
+
+Substituted the declaration as a lambda function by this declaration below of the function that 
+matches the interface ActionReducer to avoid the error from the ngc compiler:
+
+ Error: Error encountered resolving symbol values statically. Function calls are not supported.
+        Consider replacing the function or lambda with a reference to an exported function
+*/
+import {Action} from "@ngrx/store";
+
+export function counterReducer (state: number = 0, action: Action)  {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+
