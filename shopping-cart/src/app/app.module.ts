22a2a1b9948d070c2facc530c9e78335e6da7922
Rafael Sanchez <rafasanmartinez@gmail.com>
2016-11-25 21:15:25 +0100

    Effects included and complinig with AoT

--- shopping-cart/src/app/app.module.ts
+++ shopping-cart/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HomePage } from '../pages/home/home';
 import { cartReducer }  from '../reducers/cart';
 import { productsReducer } from '../reducers/products';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { ShopEffects} from '../effects/shop';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { StoreModule } from '@ngrx/store';
     StoreModule.provideStore({
         cart: cartReducer,
         products: productsReducer,
-    })
+    }),
+    EffectsModule.run(ShopEffects)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
